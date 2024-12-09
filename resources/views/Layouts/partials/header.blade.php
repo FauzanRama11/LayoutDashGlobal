0697cb7c2aa810c8d793cf02eb51cbca3b048fcc
Nabila DJ <92419049+Nabiladnjsmn@users.noreply.github.com>
2024-12-09 15:00:55 +0700

    logo

--- resources/views/Layouts/partials/header.blade.php
+++ resources/views/Layouts/partials/header.blade.php
@@ -1,7 +1,8 @@
 <div class="page-main-header">
   <div class="main-header-right row m-0">
     <div class="main-header-left">
-      
+      <div class="logo-wrapper"><a href=""><img class="img-fluid" src="{{asset('assets/images/logo/unair_age.png')}}" alt="" style="width: 100px";></a></div>
+      <div class="dark-logo-wrapper"><a href=""><img class="img-fluid" src="{{asset('assets/images/logo/dark-logo.png')}}" alt=""></a></div>
       <div class="toggle-sidebar"><i class="status_toggle middle" data-feather="align-center" id="sidebar-toggle">    </i></div>
     </div>
     <div class="left-menu-header col">
