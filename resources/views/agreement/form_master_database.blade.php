37caa0adcbdccfdda54c8c269f5896cba812cbf3
Fauzan <143669548+FauzanRama11@users.noreply.github.com>
2025-02-04 06:03:06 +0700

    Update form_master_database.blade.php

--- resources/views/agreement/form_master_database.blade.php
+++ resources/views/agreement/form_master_database.blade.php
@@ -509,7 +509,7 @@ function confirmSubmission(event) {
         function validateFileSize(input) {
                 const file = input.files[0];
                 if (file) {
-                    const maxSize = 1 * 1024 * 1024; // 1 MB
+                    const maxSize = 2.5 * 1024 * 1024; // 1 MB
                     if (file.size > maxSize) {
                         Swal.fire({
                             title: 'File too large!',
